2dcec8ff0647f6923d31c101614a81a7badf512d
ankravchenko <etercian@gmail.com>
2024-09-10 12:08:29 +0200

    fixed ranking file path for scenes

--- complexity_colour_fft.sh
+++ complexity_colour_fft.sh
@@ -8,7 +8,7 @@ python3 complexity_colour_fft.py objects "../Savoias-Dataset/Images/Objects/" ".
 
 python3 complexity_colour_fft.py art "../Savoias-Dataset/Images/Art/" "../Savoias-Dataset/Ground truth/xlsx/global_ranking_art.xlsx" fft
 
-python3 complexity_colour_fft.py scenes "../Savoias-Dataset/Images/Scenes/" "../Savoias-Dataset/Ground truth/xlsx/global_ranking_places.xlsx" fft
+python3 complexity_colour_fft.py scenes "../Savoias-Dataset/Images/Scenes/" "../Savoias-Dataset/Ground truth/xlsx/global_ranking_scenes.xlsx" fft
 
 python3 complexity_colour_fft.py infographics "../Savoias-Dataset/Images/Visualizations/" "../Savoias-Dataset/Ground truth/xlsx/global_ranking_vis.xlsx" fft
 
