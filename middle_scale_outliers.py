829f8e9ba0a9c0787c0964a68d2d0290231e941b
ankravchenko <etercian@gmail.com>
2024-09-17 18:19:09 +0200

    plotting outliers

---
+++ middle_scale_outliers.py
@@ -0,0 +1,203 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from scipy import stats
+import sys
+import os
+
+#read parameters
+subset_name='suprematism'
+subset_name=sys.argv[1]
+cg_type='fft'
+#cg_type=sys.argv[2]
+
+#pickle load
+handle=open('results/calculated_mssc/'+cg_type+'_'+subset_name+'_complexity.pickle','rb')
+df=pickle.load(handle)
+
+#remove outliers
+
+df_orig=df.copy()
+
+idx=[]
+i_idx=[]
+outliers=df.loc[idx]
+i_outliers=df.loc[i_idx]
+
+if subset_name=='suprematism':
+	idx=[50]#artifacts from poor scanning quality comparable to meaningful features
+	outliers=df.loc[idx]
+	i_idx=[13, 20, 48, 52, 53]
+	df=df.drop(idx)
+	i_outliers=df.loc[i_idx]
+	df=df.drop(i_idx)
+
+
+if subset_name=='advertisement': 
+	idx=[199]#mcdonalds logo was ranked as 0 by humans while being technically somewhat complex.  85 and 86 are just bugs!
+	outliers=df.loc[idx]
+	df=df.drop(idx)
+
+'''
+if subset_name=='infographics':
+	idx=[115, 99, 25, 117, 44, 68, 177, 140, 35]#these only have text. removing them improves performance but doesn't alter distribution shape FIXME no these are just bugs of coarse graining
+	outliers=df.loc[idx]
+	df=df.drop(idx)
+'''
+
+if subset_name=='art':
+	idx=[225, 380, 119, 30, 286, 88]
+	outliers=df.loc[idx]
+	df=df.drop(idx)
+'''
+idx=[]
+#[13,20,50,53,64]
+if subset_name=='suprematism':
+	idx=[13, 20, 48, 50, 52, 53, 64]#[50, 95, 64, 65, 52, 53, 13, 48, 20]
+elif subset_name=='advertisement':
+	#msk=df['ms_total']>0.3 
+	idx = [85, 86]#df.index[msk] 109
+elif subset_name=='art':
+	idx=[30, 88, 107, 119, 225, 380, 200]
+	[88,107,225,380]
+	#idx=[119,107,88,30,200, 225,380, 328, 286, 216, 316, 332, 86, 108, 73, 23, 285,31]
+	#idx=[328, 80, 86, 225, 286, 352, 380, 119] for tasteless images
+	#idx=[380, 225, 30, 88, 119, 286, 107] for broad lines
+#elif subset_name=='scenes':
+	#idx=[52, 22]
+#elif subset_name=='infographics':
+	#idx=[43,48,63,103,105,171,173]
+	#msk=df['ms_total']>0.5 
+	#idx = df.index[msk]
+'''
+
+
+'''
+x=range(0,df['ms_total'].to_numpy().shape[0])
+y1=df['gt'].to_numpy()
+y2=df['ms_total'].to_numpy()
+
+y1o=outliers['gt'].to_numpy()
+y2o=outliers['ms_total'].to_numpy()
+'''
+
+
+#separate df for statistics
+#regression for each separate scale
+
+#regression for each separate scale: loop
+reg_results=[]
+y=df['gt']
+features = [f"s{i}" for i in range(13)]
+
+max_r=0
+for i in range(10):
+	#regression
+	x=df[features[i]]
+	#xo=outliers['ms_total']
+	#yo=outliers['gt']
+	if x.any():
+		slope, intercept, r, p, std_err = stats.linregress(x, y)
+		reg_results.append([r,p])
+		if (r>max_r):
+			max_r=r
+	else:
+		reg_results.append([0,0])
+print("max correlation at a single scale: ", str(max_r))
+
+df_stats = pd.DataFrame(reg_results, columns=['r', 'p'])
+
+
+if not os.path.exists('results/mssc_figures/'):
+	os.mkdir('results/mssc_figures/')
+if not os.path.exists('results/mssc_figures_eps/'):
+	os.mkdir('results/mssc_figures_eps/')
+
+df_stats.to_csv('results/calculated_mssc/'+cg_type+'_'+subset_name+'_complexity_regression.csv', sep='\t')
+print(subset_name+', full: r=', str(r)+', p='+str(p))
+
+
+fr1=2
+fr2=7
+ttt=df['s'+str(fr2)]
+for i in range(fr1,fr2):
+	ttt=ttt+df['s'+str(i)]
+df['frac']=ttt
+x=df['frac']
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+freqrange=str(fr1)+'-'+str(fr2)
+print(subset_name+', '+freqrange+': r=', str(r)+', p='+str(p))
+
+frac_std=np.std(ttt)
+y1=slope * x + intercept
+
+ttt=outliers['s'+str(fr2)]
+for i in range(fr1,fr2):
+	ttt=ttt+outliers['s'+str(i)]
+outliers['frac']=ttt
+
+
+ttt=i_outliers['s'+str(fr2)]
+for i in range(fr1,fr2):
+	ttt=ttt+i_outliers['s'+str(i)]
+i_outliers['frac']=ttt
+
+
+
+#regression
+x=df['frac']
+y=df['gt']
+xo=outliers['frac']
+yo=outliers['gt']
+i_xo=i_outliers['frac']
+i_yo=i_outliers['gt']
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+y1=slope * x + intercept
+plt.clf()
+plt.scatter(x, y)
+plt.ylabel('subjective complexity',fontsize=16)
+plt.xlabel('MSSC',fontsize=16)
+plt.scatter(xo,yo,color='red', linewidth=2, alpha=1)
+plt.scatter(i_xo,i_yo,color='#f6984f', linewidth=2, alpha=1)
+plt.plot(x, y1, color='orange')
+plt.xlim([-0.01, 0.18])
+plt.title(subset_name, fontsize=16)
+plt.savefig('results/mssc_figures/'+cg_type+'_'+subset_name+'_regression_'+freqrange+'.png')
+plt.savefig('results/mssc_figures_eps/'+cg_type+'_'+subset_name+'_regression_'+freqrange+'.eps', format='eps')
+
+df_part=df[['gt','frac']]
+df_part=df_part.rename({'frac': 'MSSC'}, axis='columns')
+
+df_part.to_csv('results/calculated_mssc/'+cg_type+'_'+subset_name+'_mssc.csv', sep='\t')
+
+
+
+
+
+f = open("results/mssc_figures/"+cg_type+'_'+subset_name+'_regression_'+freqrange+'.log', "w")
+ttt=[slope, intercept, r, p, std_err]
+print("slope\tintercept\tr\tp\tstd_err", end='\n', file=f)
+print(*ttt, sep='\t', end='\n', file=f)
+f.close()
+
+
+#regression full
+x=df['ms_total']
+y=df['gt']
+#xo=outliers['ms_total']
+#yo=outliers['gt']
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+y1=slope * x + intercept
+plt.clf()
+plt.scatter(x, y)
+plt.ylabel('subjective complexity',fontsize=16)
+plt.xlabel('MSSC',fontsize=18)
+#plt.scatter(xo,yo,color='red', linewidth=2, alpha=0.5)
+plt.plot(x, y1, color='orange')
+plt.xlim([0, 0.4])
+plt.title(subset_name, fontsize=16)
+plt.savefig('results/mssc_figures/'+cg_type+'_'+subset_name+'_regression.png')
+plt.savefig('results/mssc_figures_eps/'+cg_type+'_'+subset_name+'_regression.eps', format='eps')
+
+
---
+++ middle_scale_outliers.sh
@@ -0,0 +1,7 @@
+python3 middle_scale_outliers.py scenes 
+python3 middle_scale_outliers.py art 
+python3 middle_scale_outliers.py objects 
+python3 middle_scale_outliers.py interior_design 
+python3 middle_scale_outliers.py suprematism 
+python3 middle_scale_outliers.py advertisement
+python3 middle_scale_outliers.py infographics
